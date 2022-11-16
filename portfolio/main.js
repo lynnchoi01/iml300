e636ddfa8d910fc440346dc58e0c1581c8080f52
lynnchoi01 <lynnchoi@usc.edu>
2022-11-16 14:21:52 -0800

    project 3 start

---
+++ portfolio/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $(function () {
+        $("#drag").draggable();
+    });
+
+});
+
+$(document).ready(function () {
+    gridderyV2(".entry250", "--Post-Columns");
+});
