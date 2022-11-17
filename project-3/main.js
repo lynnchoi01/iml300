879862271d47f9ad3da985b2a1ddd18c3fd8571b
lynnchoi01 <lynnchoi@usc.edu>
2022-11-16 20:45:39 -0800

    class update

--- project-3/main.js
+++ project-3/main.js
@@ -1,1 +1,9 @@
-//is there a way to make javascript redirect page (so like if this happens go to page-#.html else go to page-#.html)
+//is there a way to make javascript redirect page (so like if this happens go to page-#.html else go to page-#.html) - jQuery
+
+$(document).ready(function(){
+    $('a.answer1a').click(function(){
+        location.replace("page-3.html")
+    })
+})
+
+//IT WORKED sort of. just need to style now
